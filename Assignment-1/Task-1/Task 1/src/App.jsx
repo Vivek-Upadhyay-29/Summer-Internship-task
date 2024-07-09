733b440a9d37f94af23ec240f77ecf0cd07108ac
Vivek_Upadhyay_29 <uvivek2952003@gmail.com>
2024-07-09 21:01:01 +0530

    commit

---
+++ Assignment-1/Task-1/Task 1/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+import Carding from './Carding';
+function App() {
+
+  return (
+    <>
+    <Carding />
+    </>
+  )
+}
+
+export default App
