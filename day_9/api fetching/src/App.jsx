3bb2acf17da9e833ae0bc13caafa51288506a408
Vivek_Upadhyay_29 <uvivek2952003@gmail.com>
2024-07-14 10:24:18 +0530

    commit

---
+++ day_9/api fetching/src/App.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import{Container,Row,Col,Card} from "react-bootstrap";
+
+
+function App() {
+  const [mydata, setData] = useState([]);
+
+  const getApi = () => {
+    fetch("https://inshortsapi.vercel.app/news?category=sports")
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json.data);
+      });
+  };
+  useEffect(() => {
+    getApi();
+    const interval = setInterval(() => {
+      getApi();
+    }, 50000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <Container fluid>
+      <Row xs={1} md={3} className="g-4">
+        {mydata.map((value) => {
+          return (
+            <>
+              <Col className="container-fluid mt-4"   >
+                <Card>
+                  <Card.Img variant="top" src={value.imageUrl} height="200px" width="300px"/>
+                  <Card.Body>
+                    <Card.Title>{value.title}</Card.Title>
+                    <Card.Text>{value.content}</Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </>
+          );
+        })}
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
